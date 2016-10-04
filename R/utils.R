c46ac0da595738d6e317ce8c13a165bfb141c9f2
jsta <jstachelek@utexas.edu>
2016-10-04 13:11:49 -0400

    fill-in body of read_lagos_txt #5

--- R/utils.R
+++ R/utils.R
@@ -5,6 +5,5 @@
 #' @return data.frame
 #' @export
 load_lagos_txt <- function(file_name){
-
-
+  read.table(file_name, header = TRUE, sep = "\t", quote = "", dec = ".", strip.white = TRUE, comment.char = "")
 }
