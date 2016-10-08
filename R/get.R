d9dc123fcab4d430467cd345c6c9be374318bd5f
jsta <jstachelek@utexas.edu>
2016-10-08 11:36:24 -0400

    Create fxn to download files from GigaScience, #2

---
+++ R/get.R
@@ -0,0 +1,27 @@
+
+#' lagos_get
+#'
+#' @export
+#' @description Retrieves LAGOS flat files from GigaDB.
+#' @import rappdirs
+#' @param dest_folder file.path optional will default to the location returned by \code{\link[rappdirs]{user_data_dir}}.
+#' @import storr
+#' @examples \dontrun{
+#' lagos_get()
+#' }
+lagos_get <- function(dest_folder = NA){
+
+  # replace with LAGOS path
+  baseurl <- "ftp://climb.genomics.cn/pub/10.5524/100001_101000/100244/"
+
+  # replace with LAGOS file names
+  files <- c("Cirflux_ScalingAndModeling_canopyLevelData_GigaScience.csv",
+                 "Cirflux_ScalingAndModeling_leafLevelData_GigaScience.csv")
+
+  destdir <- rappdirs::user_data_dir("LAGOS")
+  # dir.exists(destdir)
+  dir.create(destdir, showWarnings = FALSE)
+
+  invisible(lapply(files, function(x) download.file(paste0(baseurl, x), paste0(destdir, .Platform$file.sep, x))))
+
+}
