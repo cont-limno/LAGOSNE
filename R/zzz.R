581733eefa365b6ea1f756795e9865b732f85b68
jsta <jstachelek@utexas.edu>
2016-10-09 11:27:30 -0400

    print LAGOS version and citation on load

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,15 @@
 
 .onAttach <- function(lib, pkg){
 
-  pkg.info <- drop(read.dcf(file=system.file("DESCRIPTION", package="LAGOS"),
-                            fields=c("Title","Version","Date")))
+  pkg.info <- drop(read.dcf(file = system.file("DESCRIPTION",
+                package = "LAGOS"), fields = c("Title", "Version", "Date")))
 
-  packageStartupMessage("Welcome to the LAGOS Package, Here is citation information:")
+  gigascience_cite <-
+    "Soranno, P.A., Bissell, E.G., Cheruvelil, K.S., Christel, S.T., Collins,
+    S.M., Fergus, C.E., Filstrup, C.T., Lapierre, J.F., Lottig, N.R., Oliver,
+    S.K. and Scott, C.E., 2015. Building a multi-scaled geospatial temporal
+    ecology database from disparate data sources: fostering open science and
+    data reuse. Gigascience, 4(1)"
+
+  packageStartupMessage(paste0("Welcome to LAGOS version ", pkg.info["Version"] , ". To cite LAGOS in publications use: \n", gigascience_cite))
 }
