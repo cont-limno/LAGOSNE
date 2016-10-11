72c4c5b6c8ae821f46f5a019cd229624d4c4f7b4
jsta <jstachelek@utexas.edu>
2016-10-11 16:08:39 -0400

    rm old epi-nutr test

--- tests/testthat/test-load_lagos_txt.R
+++ tests/testthat/test-load_lagos_txt.R
@@ -12,16 +12,4 @@ test_that("load_lagos_txt is equivalent to a read.table call", {
   expect_equal(lagos.program,
                load_lagos_txt('lagos_program_10541.txt'))
 
-  # epi.nutr <- read.table("../../data-raw/Limno10541/lagos_epi_nutr_10541.txt",
-  #                        header = TRUE,
-  #                        sep = "\t",
-  #                        quote = "",
-  #                        dec = ".",
-  #                        strip.white = TRUE,
-  #                        comment.char = "",
-  #                        colClasses=c(sampledate = "POSIXct"))
-  #
-  # expect_equal(epi.nutr,
-  #              load_lagos_txt('../../data-raw/Limno10541/lagos_epi_nutr_10541.txt', colClasses = c(sampledate = "POSIXct")))
-
   })
