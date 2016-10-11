d3f6d589f7755c87fb2330f5fe8020baf51627ea
jsta <jstachelek@utexas.edu>
2016-10-10 20:27:53 -0400

    add ability to compile sqlite database

--- R/compile.R
+++ R/compile.R
@@ -3,13 +3,14 @@
 #'@title Compile LAGOS data
 #'@description Compile LAGOS data from component flat files
 #'@param version character LAGOS database version string
+#'@param outformat character choice of "rds" or "sqlite"
 #'@importFrom utils read.table
 #'@import rappdirs
 #'@examples \dontrun{
-#'lagos_compile("1.054.1")
+#' lagos_compile("1.054.1", outformat = "sqlite")
 #'}
 #'
-lagos_compile <- function(version){
+lagos_compile <- function(version, outformat = "rds"){
 
   ingest <- lagos_ingest(version = version)
 
@@ -20,6 +21,22 @@ lagos_compile <- function(version){
   destdir <- rappdirs::user_data_dir("LAGOS")  # dir.exists(destdir)
   dir.create(destdir, showWarnings = FALSE)
 
-  saveRDS(list("geo" = geo, "limno" = limno, "info" = info),
-          paste0(destdir, "/data_", version, ".rds"))
+  if(outformat == "sqlite"){
+
+    # unlink(paste0(destdir, "/LAGOS.sqlite3"))
+    my_db <- dplyr::src_sqlite(paste0(destdir, "/LAGOS.sqlite3"),
+              create = TRUE)
+
+    invisible(lapply(seq_along(geo),
+      function(i, dt = geo) dplyr::copy_to(my_db, dt[[i]], names(dt)[[i]],
+      temporary = FALSE)))
+
+    invisible(lapply(seq_along(limno),
+      function(i, dt = limno) dplyr::copy_to(my_db, dt[[i]], names(dt)[[i]],
+      temporary = FALSE)))
+
+  }else{
+    saveRDS(list("geo" = geo, "limno" = limno, "info" = info),
+            paste0(destdir, "/data_", version, ".rds"))
+  }
 }
