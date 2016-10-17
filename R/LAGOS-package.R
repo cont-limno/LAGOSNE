3f52b466fe13c82281ae29ef63c79b567b334e08
jsta <jstachelek@utexas.edu>
2016-10-17 13:27:54 -0400

    add example of dataset documentation, #7

---
+++ R/LAGOS-package.R
@@ -0,0 +1,31 @@
+#' R interface to the LAGOS lakes database
+#' @name LAGOS-package
+#' @aliases LAGOS
+#' @docType package
+#' @title R interface to the LAGOS lakes database
+#' @author \email{stachel2@msu.edu}
+NULL
+
+#' Watershed Data
+#'
+#' A dataset containing information on watershed characteristics for LAGOS lakes
+#'
+#' \itemize{
+#'     \item iws_nhdid
+#'     \item iws_areaha
+#'     \item iws_perimkm
+#'     \item iws_lat latitude
+#'     \item iws_long longitude
+#'     \item iws_lakeareaha
+#'     \item iws_pct_in_usa
+#'     \item iws_ha_in_usa
+#'     \item iws_pct_in_nwi
+#'     \item iws_ha_in_nwi
+#'     \item iws_zoneid
+#'     \item iws_lagoslakeid
+#'     }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name iws
+NULL
