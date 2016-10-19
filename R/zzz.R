99a01bc4d26f13c76df489b79b24e929e82b8dc9
jsta <jstachelek@utexas.edu>
2016-10-19 18:30:13 -0400

    better citation alignment

--- R/zzz.R
+++ R/zzz.R
@@ -10,9 +10,9 @@
     S.M., Fergus, C.E., Filstrup, C.T., Lapierre, J.F., Lottig, N.R., Oliver,
     S.K., Scott, C.E., Smith, N.J., Stopyak, S., Yuan, S., Bremigan, M.T.,
     Downing, J.A., Gries, C., Henry, E.H., Skaff, N.K., Stanley, E.H.,
-    Stow, C.A., Tan, P.N., Wagner, T. and Webster, K.E. 2015. Building a multi-scaled geospatial temporal
-    ecology database from disparate data sources: fostering open science and
-    data reuse. Gigascience, 4(1)"
+    Stow, C.A., Tan, P.N., Wagner, T. and Webster, K.E. 2015. Building a
+    multi-scaled geospatial temporal ecology database from disparate data
+    sources: fostering open science and data reuse. Gigascience, 4(1)"
 
   packageStartupMessage(paste0("Welcome to LAGOS version ",
     pkg.info["Version"] , ". To cite LAGOS in publications use: \n \n",
