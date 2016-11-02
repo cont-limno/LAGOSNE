d041afbd7d37d43ef70f0ae2ed40181cb0244d99
jsta <jstachelek@utexas.edu>
2016-11-02 14:15:40 -0400

    better if else spacing

--- R/load.R
+++ R/load.R
@@ -12,13 +12,17 @@
 lagos_load <- function(version, format = "rds", fpath = NA){
 
   if(!is.na(fpath)){
+
     if(format == "sqlite"){
       dplyr::src_sqlite(fpath)
     }else{
       readRDS(fpath)
     }
+
   }else{
+
     data_dir <- rappdirs::user_data_dir("LAGOS")
+
     if(format == "sqlite"){
       sqlite_path <- paste0(data_dir, "/LAGOS.sqlite3")
       stop_if_not_exists(sqlite_path)
@@ -28,6 +32,7 @@ lagos_load <- function(version, format = "rds", fpath = NA){
       stop_if_not_exists(rds_path)
       readRDS(rds_path)
     }
+
   }
 }
 
