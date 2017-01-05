f958e834668cc15175b1f262e7a52f2c849b5265
jsta <jstachelek@utexas.edu>
2017-01-05 10:18:39 -0500

    skip backwards compatability test

--- tests/testthat/test-load_lagos_txt.R
+++ tests/testthat/test-load_lagos_txt.R
@@ -1,6 +1,8 @@
 context("load_lagos_txt")
 
 test_that("load_lagos_txt is equivalent to a read.table call", {
+  skip("This test is only necessary to check backwards compatibility with
+       legacy codebase")
   lagos.program <- read.table('lagos_program_10541.txt',
                               header=T,
                               sep = "\t",
