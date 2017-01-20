469b3cede399ea23f6d95e5baafa6c7ad1680381
jsta <jstachelek@utexas.edu>
2017-01-20 08:23:28 -0500

    add function to turn table in a spatial object, closes #32

---
+++ R/coordinates.R
@@ -0,0 +1,26 @@
+
+#' coordinatize
+#' @description Turn a LAGOS flat file into a spatial object using a reasonable
+#'  default projection
+#' @param dt data.frame
+#' @param latname character name of latitude column; default is "nhd_lat"
+#' @param longname character name of longitude column; defailt is "nhd_long"
+#' @param projstring character projection string; default is WGS84-lat-long
+#' @importFrom sp coordinates proj4string CRS
+#' @export
+#' @examples \dontrun{
+#' dt <- lagos_load("1.087.0")
+#' res <- coordinatize(dt$lake.specific)
+#'
+#' library(maps)
+#' map("state", xlim = sp::bbox(res)[c(1,3)], ylim = sp::bbox(res)[c(2,4)])
+#' sp::plot(res, add = TRUE, pch = 19, cex = 0.05)
+#' }
+coordinatize <- function(dt, latname = "nhd_lat", longname = "nhd_long",
+                         projstring = "+init=epsg:4326"){
+
+  sp::coordinates(dt) <- c(longname, latname)
+  sp::proj4string(dt) <- sp::CRS(projstring)
+
+  dt
+}
