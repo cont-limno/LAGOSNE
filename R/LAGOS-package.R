729756ca61967f11394217809ef39a898afd4be6
jsta <jstachelek@utexas.edu>
2017-02-12 17:09:09 -0500

    add scales as a classification alias, #33

--- R/LAGOS-package.R
+++ R/LAGOS-package.R
@@ -93,7 +93,7 @@ NULL
 #' @keywords datasets
 #' @name classifications
 #' @references Soranno et al. (2015) \href{https://gigascience.biomedcentral.com/articles/10.1186/s13742-015-0067-4}{Gigascience 4:28}
-#' @aliases lakes4ha.buffer100m lakes4ha.buffer500m buffer100m buffer500m iws hu12 hu8 hu4 edu county state classification
+#' @aliases lakes4ha.buffer100m lakes4ha.buffer500m buffer100m buffer500m iws hu12 hu8 hu4 edu county state classification scales
 NULL
 
 #' Land Use Land Cover (LULC) Data Frames
