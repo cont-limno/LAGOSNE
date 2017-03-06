6c64a9f7d97d0ff4bc5cb46613c161d62bf25cc1
jsta <jstachelek@utexas.edu>
2017-03-06 15:26:27 -0500

    abandoning backwards compatiabiity as of 1.087.0

--- tests/testthat/test-load_lagos_txt.R
+++ tests/testthat/test-load_lagos_txt.R
@@ -1,17 +1,17 @@
-context("load_lagos_txt")
-
-test_that("load_lagos_txt is equivalent to a read.table call", {
-  skip("This test is only necessary to check backwards compatibility with
-       legacy codebase")
-  lagos.program <- read.table('lagos_program_10541.txt',
-                              header=T,
-                              sep = "\t",
-                              quote = "",
-                              dec = ".",
-                              strip.white=T,
-                              comment.char="")
-
-  expect_equal(lagos.program,
-               load_lagos_txt('lagos_program_10541.txt'))
-
-  })
+# context("load_lagos_txt")
+#
+# test_that("load_lagos_txt is equivalent to a read.table call", {
+#   skip("This test is only necessary to check backwards compatibility with
+#        legacy codebase")
+#   lagos.program <- read.table('lagos_program_10541.txt',
+#                               header=T,
+#                               sep = "\t",
+#                               quote = "",
+#                               dec = ".",
+#                               strip.white=T,
+#                               comment.char="")
+#
+#   expect_equal(lagos.program,
+#                load_lagos_txt('lagos_program_10541.txt'))
+#
+#   })
