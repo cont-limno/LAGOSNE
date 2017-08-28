552114eca96cb8df40b5b34fd97f1c26e56756d8
jsta <jstachelek@utexas.edu>
2017-08-28 10:51:29 -0400

    attempt minimal static 2

--- tests/testthat/test-lagos_get.R
+++ tests/testthat/test-lagos_get.R
@@ -1,10 +1,12 @@
 context("lagos_get")
 
 test_that("lagos_get works", {
-  skip("Need to figure out a way to shortcut the full download")
+
   skip_on_cran()
   skip_on_travis()
   skip_on_appveyor()
+
+  skip("Need to figure out a way to shortcut the full download")
 
   lagos_get()
 
--- tests/testthat/test-lagos_load.R
+++ tests/testthat/test-lagos_load.R
@@ -1,6 +1,11 @@
 context("lagos_load")
 
 test_that("lagos_load fails well", {
+
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
   expect_error(lagos_load("gibberish"),
     paste0("Dataset not found at: ",
     paste0(lagos_path(), "data_", "gibberish", ".rds"),
