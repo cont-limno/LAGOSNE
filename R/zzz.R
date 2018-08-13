3d73802bd01b6cd4eed74c9f9e58c5ad1495d639
jsta <stachel2@msu.edu>
2018-08-13 11:57:22 -0400

    update citation info for data paper, closes #79

--- R/zzz.R
+++ R/zzz.R
@@ -6,16 +6,10 @@
                 package = "LAGOSNE"), fields = c("Title", "Version", "Date")))
 
   gigascience_cite <-
-    "Soranno, P.A., Bissell, E.G., Cheruvelil, K.S., Christel, S.T., Collins,
-    S.M., Fergus, C.E., Filstrup, C.T., Lapierre, J.F., Lottig, N.R., Oliver,
-    S.K., Scott, C.E., Smith, N.J., Stopyak, S., Yuan, S., Bremigan, M.T.,
-    Downing, J.A., Gries, C., Henry, E.H., Skaff, N.K., Stanley, E.H.,
-    Stow, C.A., Tan, P.N., Wagner, T. and Webster, K.E. 2015. Building a
-    multi-scaled geospatial temporal ecology database from disparate data
-    sources: fostering open science and data reuse. Gigascience, 4(1)"
+  "https://lagoslakes.org/how-to-cite-lagos-ne/"
 
   if(interactive()){
-    packageStartupMessage(paste0("Welcome to the LAGOSNE R package. To cite LAGOSNE in publications use: \n \n",
+    packageStartupMessage(paste0("Welcome to the LAGOSNE R package. To cite LAGOSNE in publications see: \n ",
     gigascience_cite))
   }
 }
