c08dad98d5fbc50ef48268f456723fa8190a6575
jsta <stachel2@msu.edu>
2018-11-06 08:21:12 -0500

    fix argument order

--- tests/testthat/test-query_lagos_names.R
+++ tests/testthat/test-query_lagos_names.R
@@ -4,6 +4,6 @@ test_that("query_lagos_names works", {
 
   dt <- readRDS("lagos_test_subset.rds")
 
-  expect_equal(query_lagos_names(dt, "chla"), "epi_nutr")
+  expect_equal(query_lagos_names("chla"), "epi_nutr")
 
   })
