2cf6e604eff43878199a79a071139666b81adc89
jsta <stachel2@msu.edu>
2018-11-22 09:32:59 -0500

    use dt subset

--- tests/testthat/test-query_lagos_names.R
+++ tests/testthat/test-query_lagos_names.R
@@ -3,7 +3,6 @@ context("query_lagos_names")
 test_that("query_lagos_names works", {
 
   dt <- readRDS("lagos_test_subset.rds")
+  expect_equal(query_lagos_names("chla", dt = dt), "epi_nutr")
 
-  expect_equal(query_lagos_names("chla"), "epi_nutr")
-
-  })
+})
