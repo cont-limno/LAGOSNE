1b2d47126c864bb1cf2bd097af11711ec5a5067b
jsta <stachel2@msu.edu>
2019-05-02 16:08:49 -0400

    rm junk logo file

--- R/.logo.R
+++
@@ -1,7 +0,0 @@
-# logo
-hexSticker::sticker("inst/lagos-transparent.png", package = "LAGOSNE",
-                    s_width = 1, s_height = 1, s_x = 1, s_y = 1, p_size = 0,
-                    p_color = "#231f20", h_fill = "#231f20", h_color = "#231f20",
-                    filename = "inst/lagos_hex.png")
-
-usethis::use_logo("inst/lagos_hex.png")
