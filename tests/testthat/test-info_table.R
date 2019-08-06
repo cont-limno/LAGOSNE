d6a581ddd0337805069dd95f078101ffb0c0898f
jsta <stachel2@msu.edu>
2019-08-06 12:54:55 -0400

    add info_table test

---
+++ tests/testthat/test-info_table.R
@@ -0,0 +1,10 @@
+context("info_table")
+
+test_that("info_table works", {
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
+  lg <- lagosne_load("1.087.3")
+  expect_s3_class(LAGOSNE:::info_table(lg), "data.frame")
+})
