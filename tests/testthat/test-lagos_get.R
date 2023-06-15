63ed286740514f3e598b9730f506fe5099598063
jsta <stachel2@msu.edu>
2023-06-15 15:48:31 -0600

    test SSL connect-able-ness, #114

--- tests/testthat/test-lagos_get.R
+++ tests/testthat/test-lagos_get.R
@@ -1,5 +1,15 @@
 context("lagos_get")
 
+test_that("lagosne_get pasta urls are SSL connect-able (#114)", {
+
+  skip_on_cran()
+
+  pasta_baseurl <- "http://pasta.lternet.edu/package/data/eml/edi/"
+  locus_base_pasta <- paste0(pasta_baseurl, "100/4")
+  id <- readLines(locus_base_pasta)
+  expect_equal(id, "ce274065dfb5b453c5696f715fe4e269")
+})
+
 test_that("lagosne_get urls are active", {
 
   skip_on_cran()
@@ -7,7 +17,7 @@ test_that("lagosne_get urls are active", {
   # LAGOSNE_lakeslocus101.csv
   expect_true(
     LAGOSNE:::url_exists(paste0("https://portal.edirepository.org/nis/",
-    "dataviewer?packageid=edi.100.4&entityid=ce274065dfb5b453c5696f715fe4e269"))
+      "dataviewer?packageid=edi.100.4&entityid=ce274065dfb5b453c5696f715fe4e269"))
   )
 })
 
@@ -17,12 +27,12 @@ test_that("external download urls are active", {
   # collins et al. 2017
   expect_true(
     LAGOSNE:::url_exists(paste0("http://pasta.lternet.edu/package/data/eml/",
-                             "knb-lter-ntl/332/5/d9e3a388e566a2428a93b982a000e2c4"))
+      "knb-lter-ntl/332/5/d9e3a388e566a2428a93b982a000e2c4"))
   )
 
   # oliver et al. 2015
   expect_true(
     LAGOSNE:::url_exists(paste0("http://pasta.lternet.edu/package/data/eml/",
-                             "knb-lter-ntl/320/4/4a283c25f3548c0f78d8a01658e4a353"))
+      "knb-lter-ntl/320/4/4a283c25f3548c0f78d8a01658e4a353"))
   )
 })
